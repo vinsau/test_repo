4703f0a44a6ad77214d0ea91f68fedcd116ff81e
gauciv <vince.au23@gmail.com>
2025-05-02 11:18:22 +0800

    Fix: errors

--- src/main/java/com/example/test_repo/HelloApplication.java
+++ src/main/java/com/example/test_repo/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
   @Override
   public void start(Stage stage) throws IOException {
-    FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+    FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/test_repo/hello-view.fxml"));
     Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-    stage.setTitle("Hello, World! Whatt");
+    stage.setTitle("Hello, World!");
     stage.setScene(scene);
     stage.show();
   }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,7 @@ module com.example.test_repo {
   requires javafx.controls;
   requires javafx.fxml;
 
-  opens com.example.test_repo to
-      javafx.fxml;
+  opens com.example.test_repo to javafx.fxml;
 
   exports com.example.test_repo;
 }
